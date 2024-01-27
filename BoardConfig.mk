83a137c453d56318ce98e5e8afb077f8febfd14e
Arian <arian.kulmer@web.de>
2024-01-27 11:10:12 +0100

    diting: Add regulator, touch, haptics and audio kernel modules
    Change-Id: I33dd7fae555274ddf3dffd530df494b49469ab50

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -12,6 +12,22 @@ include vendor/xiaomi/diting/BoardConfigVendor.mk
 
 DEVICE_PATH := device/xiaomi/diting
 
+# Kernel
+device_second_stage_modules := \
+    fts_touch_spi.ko \
+    goodix_fod.ko \
+    gt9916r.ko \
+    wl2866d.ko \
+    qcom-hv-haptics.ko
+
+device_vendor_dlkm_exclusive_modules := \
+    cs35l41_dlkm.ko
+
+BOARD_VENDOR_RAMDISK_RECOVERY_KERNEL_MODULES_LOAD += $(device_second_stage_modules)
+BOARD_VENDOR_KERNEL_MODULES_LOAD += $(device_second_stage_modules) $(device_vendor_dlkm_exclusive_modules)
+
+BOOT_KERNEL_MODULES += $(device_second_stage_modules)
+
 # Properties
 TARGET_SYSTEM_PROP += $(DEVICE_PATH)/properties/system.prop
 TARGET_VENDOR_PROP += $(DEVICE_PATH)/properties/vendor.prop
