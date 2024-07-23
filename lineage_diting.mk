a0d7786ab07269e90741b73a49499b92fee13055
Jens Reidel <adrian@travitia.xyz>
2024-07-23 11:51:35 +0200

    diting: Update blobs from V816.0.5.0.ULFMIXM
    Signed-off-by: Jens Reidel <adrian@travitia.xyz>

--- lineage_diting.mk
+++ lineage_diting.mk
@@ -20,10 +20,10 @@ PRODUCT_SYSTEM_NAME := diting_global
 PRODUCT_SYSTEM_DEVICE := diting
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="diting_global-user 14 UKQ1.230917.001 V816.0.4.0.ULFMIXM release-keys" \
+    PRIVATE_BUILD_DESC="diting_global-user 14 UKQ1.230917.001 V816.0.5.0.ULFMIXM release-keys" \
     TARGET_DEVICE=$(PRODUCT_SYSTEM_DEVICE) \
     TARGET_PRODUCT=$(PRODUCT_SYSTEM_NAME)
 
-BUILD_FINGERPRINT := Xiaomi/diting_global/diting:14/UKQ1.230917.001/V816.0.4.0.ULFMIXM:user/release-keys
+BUILD_FINGERPRINT := Xiaomi/diting_global/diting:14/UKQ1.230917.001/V816.0.5.0.ULFMIXM:user/release-keys
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
