9930f2dc9f1c7ce3f8cc06c3fc19b3f771da35eb
Michael Bestas <mkbestas@lineageos.org>
2024-08-27 22:04:35 +0300

    diting: Sync extract scripts with templates
    Change-Id: I98a36854c5a0c0390592911bb2558b34eff77efc

--- extract-files.sh
+++ extract-files.sh
@@ -9,27 +9,41 @@
 function blob_fixup() {
     case "${1}" in
         vendor/etc/camera/pureView_parameter.xml)
+            [ "$2" = "" ] && return 0
             sed -i 's/=\([0-9]\+\)>/="\1">/g' "${2}"
             ;;
         vendor/etc/camera/diting_enhance_motiontuning.xml|vendor/etc/camera/diting_motiontuning.xml)
+            [ "$2" = "" ] && return 0
             sed -i 's/xml=version/xml version/g' "${2}"
             ;;
         vendor/lib64/libalNN_snpe.so)
+            [ "$2" = "" ] && return 0
             "${PATCHELF_0_17_2}" --set-soname "libalNN_snpe.so" "${2}"
             ;;
         vendor/lib64/hw/audio.primary.taro-diting.so)
+            [ "$2" = "" ] && return 0
             "${PATCHELF_0_17_2}" --set-soname "audio.primary.taro-diting.so" "${2}"
             ;;
         vendor/lib64/hw/fingerprint.goodix_fod.default.so)
+            [ "$2" = "" ] && return 0
             "${PATCHELF_0_17_2}" --set-soname "fingerprint.goodix_fod.default.so" "${2}"
             ;;
         vendor/lib64/libcamximageformatutils.so)
+            [ "$2" = "" ] && return 0
             "${PATCHELF_0_17_2}" --replace-needed "vendor.qti.hardware.display.config-V2-ndk_platform.so" "vendor.qti.hardware.display.config-V2-ndk.so" "${2}"
             ;;
         vendor/lib64/libkaraokepal.so)
+            [ "$2" = "" ] && return 0
             "${PATCHELF_0_17_2}" --replace-needed "audio.primary.taro.so" "audio.primary.taro-diting.so" "${2}"
             ;;
+        *)
+            return 1
+            ;;
     esac
+}
+
+function blob_fixup_dry() {
+    blob_fixup "$1" ""
 }
 
 # If we're being sourced by the common script that we called,
@@ -43,5 +57,6 @@ set -e
 export DEVICE=diting
 export DEVICE_COMMON=sm8450-common
 export VENDOR=xiaomi
+export VENDOR_COMMON=${VENDOR}
 
-"./../../${VENDOR}/${DEVICE_COMMON}/extract-files.sh" "$@"
+"./../../${VENDOR_COMMON}/${DEVICE_COMMON}/extract-files.sh" "$@"
