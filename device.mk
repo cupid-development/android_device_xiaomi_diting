0ea710805f14a99119edfe72dc28811a44fb2224
Arian <arian.kulmer@web.de>
2024-08-29 20:48:29 +0200

    diting: Move sensors hals.conf back to vendor repo
    sensors.xiaomi is added by common tree now
    
    Change-Id: I541b656304bfde478966c84115d94bc989742149

--- device.mk
+++ device.mk
@@ -26,10 +26,6 @@ PRODUCT_PACKAGES += \
     WifiResDiting \
     WifiResDitingCN
 
-# Sensors
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/sensors/hals.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/hals.conf
-
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
