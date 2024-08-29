41106b3d7a43a2f12889965a963349e5a6113d61
Arian <arian.kulmer@web.de>
2024-08-29 23:28:16 +0200

    diting: Rename audio.primary.taro to audio.primary.taro-diting to avoid conflicts
    Change-Id: I1d7012df4acc1e3b9180eb8523ebe77ffc8a35e9

--- extract-files.sh
+++ extract-files.sh
@@ -17,11 +17,17 @@ function blob_fixup() {
         vendor/lib64/libalNN_snpe.so)
             "${PATCHELF_0_17_2}" --set-soname "libalNN_snpe.so" "${2}"
             ;;
+        vendor/lib64/hw/audio.primary.taro-diting.so)
+            "${PATCHELF_0_17_2}" --set-soname "audio.primary.taro-diting.so" "${2}"
+            ;;
         vendor/lib64/hw/fingerprint.goodix_fod.default.so)
             "${PATCHELF_0_17_2}" --set-soname "fingerprint.goodix_fod.default.so" "${2}"
             ;;
         vendor/lib64/libcamximageformatutils.so)
             "${PATCHELF_0_17_2}" --replace-needed "vendor.qti.hardware.display.config-V2-ndk_platform.so" "vendor.qti.hardware.display.config-V2-ndk.so" "${2}"
+            ;;
+        vendor/lib64/libkaraokepal.so)
+            "${PATCHELF_0_17_2}" --replace-needed "audio.primary.taro.so" "audio.primary.taro-diting.so" "${2}"
             ;;
     esac
 }
