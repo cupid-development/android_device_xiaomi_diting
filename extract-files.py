d0ce61a8c314247533d7fdb6ec191da9d3dca98f
LuK1337 <priv.luk@gmail.com>
2024-12-10 23:07:45 +0100

    diting: Enable elfchecks for camera libs
    Reference: I23190f0b83c9e28aa2624a7575544b0eaf5c83c8
    Change-Id: I18b0b07bf3a435dd449106653e3ca458b4f07b69

--- extract-files.py
+++ extract-files.py
@@ -48,6 +48,19 @@ blob_fixups: blob_fixups_user_type = {
     'vendor/lib64/hw/com.qti.chi.override.so': blob_fixup().add_needed(
         'libprocessgroup_shim.so'
     ),
+    (
+        'vendor/lib64/libalAILDC.so',
+        'vendor/lib64/libalhLDC.so',
+        'vendor/lib64/libalLDC.so',
+        'vendor/lib64/libTrueSight.so',
+    ): blob_fixup()
+    .clear_symbol_version('AHardwareBuffer_allocate')
+    .clear_symbol_version('AHardwareBuffer_describe')
+    .clear_symbol_version('AHardwareBuffer_isSupported')
+    .clear_symbol_version('AHardwareBuffer_lock')
+    .clear_symbol_version('AHardwareBuffer_lockPlanes')
+    .clear_symbol_version('AHardwareBuffer_release')
+    .clear_symbol_version('AHardwareBuffer_unlock'),
     'vendor/lib64/libcamxcommonutils.so': blob_fixup().add_needed(
         'libprocessgroup_shim.so'
     ),
